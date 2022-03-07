c3f01e12556500c63cd2c727fb6a31e976d15e92
CephasSvosve <46489537+CephasSvosve@users.noreply.github.com>
2022-03-07 20:58:51 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,8 +13,8 @@ int main() {
 //set overal wealth on the market
 
 double market_wealth = 1.68e10;
-double shares_outstanding = 0.8*market_wealth/300;
-double market_cash = 0.2*market_wealth;
+double shares_outstanding = 0.4*market_wealth/300;
+double market_cash = 0.6*market_wealth;
 
 //Define the price setter
 price_setter market;
@@ -35,8 +35,9 @@ market.initial_wealth(0, shares_outstanding);
 
 
 //initialize traders
-funds  aggressive_value
-            , averse_value
+funds
+aggressive_value
+            ,averse_value
                 ,aggressive_noisy
                     , averse_noisy
                         ,aggressive_momentum
@@ -46,12 +47,13 @@ funds  aggressive_value
                 ;
 
 
-vector<funds*> participants = {&aggressive_value
-                                    , &averse_value
+vector<funds*> participants = {
+                        &aggressive_value
+                                    ,&averse_value
                                         , &aggressive_noisy
                                             , &averse_noisy
                                                 , &aggressive_momentum
-                                                    , &averse_momentum
+                                                    ,&averse_momentum
                                                         , &aggressive_growth
                                                              , &averse_growth
                                             };
@@ -66,6 +68,8 @@ int investor_population = participants.size();
     }
 
 
+            
+            
     aggressive_value.trade_strategy(funds::aggressive_fundamental_value);
     aggressive_value.set_reb_period(63);
     aggressive_value.initial_wealth((1./investor_population)*market_cash,0.);
@@ -73,13 +77,13 @@ int investor_population = participants.size();
     averse_value.trade_strategy(funds::fundamental_value);
     averse_value.set_reb_period(63);
     averse_value.initial_wealth((1./investor_population)*market_cash,0.);
-
+//
     aggressive_growth.trade_strategy(funds::aggressive_growth);
-    aggressive_growth.set_reb_period(63);
-    aggressive_growth.initial_wealth((1./investor_population)*market_cash,0.);
+    aggressive_growth.set_reb_period(252);
+    aggressive_growth.initial_wealth((1./(investor_population))*market_cash,0.);
 
     averse_growth.trade_strategy(funds::growth);
-    averse_growth.set_reb_period(63);
+    averse_growth.set_reb_period(252);
     averse_growth.initial_wealth((1./investor_population)*market_cash,0.);
 
 
@@ -94,7 +98,7 @@ int investor_population = participants.size();
 
     aggressive_momentum.trade_strategy(funds::aggressive_momentum_investment);
     aggressive_momentum.set_reb_period(21);
-    aggressive_momentum.initial_wealth((1./investor_population)*market_cash,0.);
+    aggressive_momentum.initial_wealth((1./(investor_population))*market_cash,0.);
 
     averse_momentum.trade_strategy(funds::momentum_investment);
     averse_momentum.set_reb_period(21);
@@ -115,7 +119,10 @@ myfile.open ("NewMarketEcology.csv");
 
 while(watch.current_time() < 10000){
 
-
+    //update traders balances before orders
+    for(auto &p : participants){
+        p->balance_cf(watch.current_time());
+    }
 
 int g = watch.current_time();
 
@@ -130,13 +137,15 @@ int kj = watch.current_time();
 //std::cout<< watch.current_time() << ". ";
 
 for(auto &[k,v] : market.tradeable_assets()){
+    myfile<<watch.current_time()<<","<<(get<1>(v.get_price())+get<2>(v.get_price()))/2.<<",";
+    myfile<<watch.current_time()<<","<<v.get_earnings(watch.current_time())<<",";
         if(k==2){
             double st=0;
-                myfile<<watch.current_time()<<","<<(get<1>(v.get_price())+get<2>(v.get_price()))/2<<","<<v.get_dividend(watch.current_time())<<","<<market.wealth<<",";
+
                 for(auto &[x,y]:market.trading_institutions){
                     myfile << y->stocks_at_hand.find(2)->second<<",";
-                     st += y->stocks_at_hand.find(2)->second;
-                    std::cout <<y->get_identifier() <<" "<<y->stocks_at_hand.find(2)->second<<",";
+                     st +=  y->stocks_at_hand.find(2)->second;
+                    std::cout <<y->get_identifier() <<" "<<y->wealth<<",";
 
                 }
             myfile<< market.stocks_at_hand.find(2)->second<<",";
@@ -148,17 +157,14 @@ for(auto &[k,v] : market.tradeable_assets()){
 }
 //    std::cout<<std::endl;
     myfile << std::endl;
+
     market.clear();
     watch.tick();
 
-    //update traders balances before orders
-    for(auto &p : participants){
-        p->balance_cf(watch.current_time());
-    }
+
 
 }
 
 myfile.close();
-
     return 0;
 }
