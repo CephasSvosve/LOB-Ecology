909ee5296478077778309e0c1832181aa148f0ea
CephasSvosve <46489537+CephasSvosve@users.noreply.github.com>
2022-05-12 17:53:46 +0200

    Update market_watch.cpp

--- market_watch.cpp
+++ market_watch.cpp
@@ -4,11 +4,38 @@
 #include "market_watch.h"
 #include <iostream>
 
-void market_watch::tick(){
-    this->lower++;
-    this->upper++;
+void market_watch::start() {
+    start_time = std::chrono::high_resolution_clock::now();
 }
 
-int market_watch::current_time(){
-    return this->lower;
+
+double
+market_watch::current_time(){
+    auto stop = std::chrono::high_resolution_clock::now();
+
+    switch (day_length) {
+        case 0:
+            return duration_cast<std::chrono::microseconds>(stop - start_time).count();
+        case 1:
+            return duration_cast<std::chrono::milliseconds>(stop - start_time).count();
+        case 2:
+            return duration_cast<std::chrono::seconds>(stop - start_time).count();
+        case 3:
+            return duration_cast<std::chrono::minutes>(stop - start_time).count();
+
+            // return (end_time-start_time)/std::chrono::miiseconds(1);
+
+
+    }
+
+}
+
+void
+market_watch::reset(){
+    start_time = std::chrono::high_resolution_clock::now();
+}
+
+time_point<std::chrono::steady_clock, std::chrono::duration<long long int, std::ratio<1, 1000000000>>>
+market_watch::get_creation_time() {
+    return creation_time;
 }
