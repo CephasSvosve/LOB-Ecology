8fc1add68cb25e2a19940ec48e74b6f5acc27f2d
CephasSvosve <46489537+CephasSvosve@users.noreply.github.com>
2022-05-12 17:52:05 +0200

    Update funds.h

--- funds.h
+++ funds.h
@@ -15,6 +15,7 @@
 #include "stochastic_math.h"
 #include <random>
 #include "order.h"
+#include <fstream>
 
 
 
@@ -24,88 +25,205 @@ using namespace std;
 
 class funds
         :public fund
-     {
+{
 private:
-    int window_size = 200;
+    int window_size = 500;
+    int value_window = 504;
+    map<int,int> rebalancing_count;
+    map<int,int> day_count;
+
 
 public:
-funds();
+    funds();
 
-enum trading_strategy{aggressive_fundamental_value=0
-                            , aggressive_noise=1
-                                , aggressive_momentum_investment=2
-                                    , aggressive_growth = 3
-                                        , aggressive_index =4
-                                            , fundamental_value =5
-                                                , noise = 6
-                                                    , momentum_investment = 7
-                                                        , growth = 8
-                                                            ,index = 9
+    std::ofstream noisefile1;
+    const MatrixXd dX = generateWhiteNoise1(6,3,10000);
 
 
-                                       };
+    enum trading_strategy{
+        aggressive_fundamental_value=4
+        , aggressive_noise=5
+        , aggressive_momentum_investment=6
+        , aggressive_growth = 7
+        , aggressive_index =8
 
 
 
-
-trading_strategy fund_philosophy;
-
-void trade_strategy(trading_strategy tradingStrategy);
-
-
-map<int, order>
-invest();
-
-
-static int concat(int a, int b);
-
-static MatrixXd
-generateWhiteNoise1(int seed, int rows, int columns);
-
-
-static double
-lateralcorrcoef(VectorXd a);
-
-
-std::map<int,order>
-ag_value_demand(),
+    };
 
 
 
-ag_growth_demand(),
+
+    trading_strategy fund_philosophy;
+
+    void trade_strategy(trading_strategy tradingStrategy);
 
 
-ag_momentum_demand(),
+    map<int, order>
+    invest();
 
 
+    static int concat(int a, int b);
 
-ag_noise_demand(),
-
-
-
-ag_index_demand(),
+    static MatrixXd
+    generateWhiteNoise1(int seed, int rows, int columns);
 
 
-
-value_demand(),
-
-
-
-growth_demand(),
-
-
-momentum_demand(),
+    std::map<int,order>
+            ag_value_demand(),
 
 
 
-noise_demand(),
+            ag_growth_demand(),
+
+
+            ag_momentum_demand(),
 
 
 
-index_demand();
+            ag_noise_demand(),
+
+
+
+            ag_index_demand(),
+
+
+
+            value_demand(),
+
+
+
+            growth_demand(),
+
+
+            momentum_demand(),
+
+
+
+            noise_demand(),
+
+
+
+            index_demand();
 
 
 
 
 };
 #endif //UNTITLED30_FUNDS_H
+
+
+
+
+
+////
+//// Created by Cephas Svosve on 21/12/2021.
+////
+//
+//#ifndef UNTITLED30_FUNDS_H
+//#define UNTITLED30_FUNDS_H
+//#include "value_investor.h"
+//#include "momentum_investor.h"
+//#include "noisy_investor.h"
+//#include <tuple>
+//#include <boost/random.hpp>
+//#include <map>
+//#include <Eigen/Dense>
+//#include "market_watch.h"
+//#include "stochastic_math.h"
+//#include <random>
+//#include "order.h"
+//
+//
+//
+//
+//
+//using namespace std;
+//
+//class funds
+//        :public fund
+//     {
+//private:
+//    int window_size = 200;
+//
+//public:
+//funds();
+//
+//enum trading_strategy{aggressive_fundamental_value=4
+//                            , aggressive_noise=5
+//                                , aggressive_momentum_investment=6
+//                                    , aggressive_growth = 7
+//                                        , aggressive_index =8
+//                                            , fundamental_value =9
+//                                                , noise = 10
+//                                                    , momentum_investment = 11
+//                                                        , growth = 12
+//                                                            ,index_ = 13
+//
+//
+//                                       };
+//
+//
+//
+//
+//trading_strategy fund_philosophy;
+//
+//void trade_strategy(trading_strategy tradingStrategy);
+//
+//
+//map<int, order>
+//invest();
+//
+//
+//static int concat(int a, int b);
+//
+//static MatrixXd
+//generateWhiteNoise1(int seed, int rows, int columns);
+//
+//
+//static double
+//lateralcorrcoef(VectorXd a);
+//
+//
+//std::map<int,order>
+//ag_value_demand(),
+//
+//
+//
+//ag_growth_demand(),
+//
+//
+//ag_momentum_demand(),
+//
+//
+//
+//ag_noise_demand(),
+//
+//
+//
+//ag_index_demand(),
+//
+//
+//
+//value_demand(),
+//
+//
+//
+//growth_demand(),
+//
+//
+//momentum_demand(),
+//
+//
+//
+//noise_demand(),
+//
+//
+//
+//index_demand();
+//
+//
+//
+//
+//};
+//#endif //UNTITLED30_FUNDS_H
